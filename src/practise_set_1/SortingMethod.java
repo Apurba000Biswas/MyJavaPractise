aef94280c93ad1546fde0799a72acc4385536649
Apurba Biswas <apurba10024@gmail.com>
2018-11-14 15:51:26 +0600

    1.1 Added Sorting Method for String, Array, ArrayList

--- src/practise_set_1/SortingMethod.java
+++ src/practise_set_1/SortingMethod.java
@@ -1,22 +1,51 @@
 package practise_set_1;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
+import java.util.Collections;
 
 public class SortingMethod {
 	
 	public static void main(String[] args) {
 		String[] words = {"foo", "bar", "baz", "ball"};
+		int[] intArray = {23, 45, 1, 32, 67, 99, 102, 21};
+		List<Integer> list = new ArrayList<>();
+		list.add(12);
+		list.add(11);
+		list.add(100);
+		list.add(67);
+		list.add(34);
 		SortingMethod mObject = new SortingMethod();
 		mObject.stringSorting(words);
+		mObject.arraySorting(intArray);
+		mObject.arrayListSorting(list);
 		
 	}
 	
 	
-	public void stringSorting(String[] str) {
+	private void stringSorting(String[] str) {
 		Arrays.sort(str);
 		for(String s: str) {
 			System.out.println(s);
 		}
+		System.out.println("");
+	}
+	
+	private void arraySorting(int[] ar) {
+		Arrays.sort(ar);
+		for(int i : ar) {
+			System.out.println(i);
+		}
+		System.out.println("");
+	}
+	
+	private void arrayListSorting(List<Integer> list) {
+		Collections.sort(list);
+		for(int i : list) {
+			System.out.println(i);
+		}
+		System.out.println("");
 	}
 
 }
