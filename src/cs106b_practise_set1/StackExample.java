8fe22c45800fedd833e11d85ceb081370f1e4700
Apurba <apurba10024@gmail.com>
2019-02-10 22:45:42 +0600

    2.3 Added Stack Example

---
+++ src/cs106b_practise_set1/StackExample.java
@@ -0,0 +1,50 @@
+package cs106b_practise_set1;
+
+import java.util.ArrayList;
+import java.util.Stack;
+
+public class StackExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		StackExample obj = new StackExample();
+		obj.run();
+	}
+	
+	private void run(){
+		ArrayList<String> list = new ArrayList<>();
+		list.add("a");
+		list.add("b");
+		list.add("c");
+		list.add("b");
+		list.add("d");
+		list.add("b");
+		list.add("e");
+		System.out.println("Original List");
+		printList(list);
+		
+		Stack<String> stack = putInStack(list, "b");
+		while(!stack.isEmpty()){
+			System.out.println(stack.pop());
+		}
+	}
+	
+	private void printList(ArrayList<String> list){
+		for(String str: list){
+			System.out.print(str  + " ");
+		}
+		System.out.println();
+	}
+	
+	private Stack<String> putInStack(ArrayList<String> list, String str){
+		Stack<String> stack = new Stack<>();
+		for(String s: list){
+			if(s.equals(str)){
+				stack.push(s);
+			}
+		}
+		return stack;
+	}
+
+}
