bfe3d1470c201bd7b366192daa61c4869e3fcb3f
Apurba <apurba10024@gmail.com>
2019-02-11 13:36:06 +0600

    2.6 Added FileReading Example

---
+++ src/cs106b_practise_set1/WordReadingFromTextFile.java
@@ -0,0 +1,46 @@
+package cs106b_practise_set1;
+
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Scanner;
+
+public class WordReadingFromTextFile {
+	
+	
+	private static final String FILE_NAME = "D:/Project/MyJavaPractise/rand_storry.txt";
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		WordReadingFromTextFile obj = new WordReadingFromTextFile();
+		obj.readStorry();
+	}
+	
+	private void readStorry(){
+		Path path = Paths.get(FILE_NAME);
+		try{
+			Scanner scanner = new Scanner(path);
+			//readLineByLine(scanner);
+			readWordByWord(scanner);
+		}catch(IOException e){
+			System.out.println("????  " + e.getMessage());
+		}
+	}
+	
+	private void readLineByLine(Scanner scanner){
+		while(scanner.hasNextLine()){
+		    String line = scanner.nextLine();
+		    System.out.println(line);
+		}
+		scanner.close();
+	}
+	
+	private void readWordByWord(Scanner scanner){
+		while(scanner.hasNext()){
+			String word = scanner.next();
+			System.out.println(word);
+		}
+        scanner.close();
+	}
+
+}
