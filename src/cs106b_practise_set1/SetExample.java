4a2b6a85174f450e3422687d2f08bcbab709f342
Apurba <apurba10024@gmail.com>
2019-03-08 17:06:33 +0600

    2.8 Added Set Example

---
+++ src/cs106b_practise_set1/SetExample.java
@@ -0,0 +1,40 @@
+package cs106b_practise_set1;
+
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
+
+public class SetExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		SetExample obj = new SetExample();
+		obj.run();
+	}
+	
+	private void run(){
+		Set<String> set = new HashSet<>();
+		buildSet(set);
+		printSet(set);
+		System.out.println("******This is default print********");
+		System.out.println(set);
+		
+	}
+	
+	private void buildSet(Set<String> set){
+		set.add("Apurba");
+		set.add("Biswas");
+		set.add("Test");
+		set.add("Hey");
+		set.add("Apurba");
+		set.add("Apurba");
+	}
+	
+	private void printSet(Set<String> set){
+		Iterator<String> it = set.iterator();
+		while(it.hasNext()){
+			System.out.println(it.next());
+		}
+	}
+
+}
